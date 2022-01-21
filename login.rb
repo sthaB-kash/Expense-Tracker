6d305c987364067b6f44e5b198b6dc25f01a2467
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-01-22 00:12:18 +0545

    accessing user's info. for user's profile

--- login.rb
+++ login.rb
@@ -1,3 +1,4 @@
+require_relative './user_profile'
 
 class Login
 
@@ -16,17 +17,19 @@ class Login
   private 
 
   def check_credentials(con)
-    query = "SELECT uname, pw FROM users WHERE uname = '%s';" % [@username]
+    query = "SELECT * FROM users WHERE uname = '%s';" % [@username]
     begin
-      if con.exec(query)[0]['pw'] == pw
-        puts "logged in"
+      user = con.exec(query) 
+      if user[0]['pw'] == pw
+        UserProfile.new(con, user[0])
       else
         puts "Username or password invalid!"
+        sleep(2)
       end
     rescue => exception
       puts "user not found or something went wrong!"
+      puts exception.message
+      sleep(2)
     end
-    readline()
   end 
-
 end
